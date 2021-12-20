90f017c889c5d5cdebfbb220850543cd47d056cd
gerardogonza <irjecogerardo@gmail.com>
2021-12-19 22:47:41 -0600

    yap

--- practica1/archivosSQL/consultas.sql
+++ practica1/archivosSQL/consultas.sql
@@ -2,7 +2,7 @@ SELECT COUNT(*) FROM project WHERE YEAR(start_actual_isodate) = 2011;
 
 SELECT idProyecto,DATEDIFF(end_actual_isodate,start_actual_isodate)as ProyectoMasDuracion,project_title FROM project ORDER BY 2 DESC LIMIT 1;
 
-SELECT idProyecto,DATEDIFF(end_actual_isodate,start_actual_isodate)as 
+SELECT idProyecto,project_title,DATEDIFF(end_actual_isodate,start_actual_isodate)as 
 ProyectoMenosDuracion FROM project  WHERE end_actual_isodate 
 IS NOT NULL ORDER BY ProyectoMenosDuracion ASC LIMIT 1;
 
@@ -23,8 +23,11 @@ FROM transactions ORDER BY 1 DESC LIMIT 5;
 
 SELECT SUM(transaction_value) FROM transactions;
 	
-SELECT transactions.id,project_title,transactions.transaction_value from project,transactions WHERE 
-project_title LIKE "%RI-East Africa Public He%" AND transactions.project_id=project.idProyecto 
+SELECT transactions.id,project_title,transactions.transaction_value from project,transactions 
+WHERE 
+    project_title LIKE "%RI-East Africa Public He%" 
+AND 
+    transactions.project_id=project.idProyecto 
 ORDER BY 2 DESC LIMIT 3
 ;
 
